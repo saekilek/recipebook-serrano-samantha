a7f55f5de3465ecaf4509ab4362661b05273fe79
saekilek <samsrrano271@gmail.com>
2025-03-08 16:07:38 +0800

    Created the necessary models for the database.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse("ledger:ingredients", args=[str(self.id)])
+
+class Recipe(models.Model):
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse("ledger:recipe_ingredients", args=[str(self.id)])
+
+class RecipeIngredient(models.Model):
+  quantity = models.CharField(max_length=200)
+  ingredient = models.ForeignKey(
+    Ingredient,
+    on_delete=models.CASCADE
+  )
+  recipe = models.ForeignKey(
+    Recipe,
+    on_delete=models.CASCADE
+  )
