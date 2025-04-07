c8aed37ec16e089c8ad5d95d21e237d60a0e5555
saekilek <samsrrano271@gmail.com>
2025-04-08 00:32:53 +0800

    Modified the urls to access pages leading to add recipe or add image pages and enabled access to media files

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import recipe_list, recipe_ingredients, home
+from .views import recipe_list, recipe_ingredients, home, add_recipe, add_recipe_image
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
    path('',home,name='home'),
    path('recipes/list/', recipe_list, name='recipe_list'),
-   path('recipe/<int:recipe_id>/', recipe_ingredients, name='recipe_ingredients')
+   path('recipe/<int:recipe_id>/', recipe_ingredients, name='recipe_ingredients'),
+   path('recipe/add/',add_recipe, name='add_recipe'),
+   path('recipe/<int:recipe_id>/add_image',add_recipe_image, name='add_recipe_image'),
 ]
 
 app_name = 'ledger'
+
+if settings.DEBUG:
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
